796cfc4bb658e14d19577310d8d5e1f617ea61ec
Daniel Kończyk <daniel@konczyk.net>
2025-12-22 01:05:52 +0100

    Decode integers

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,17 @@ struct Args {
 
 
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if let Some((n, s)) = encoded_value
+    if let Some(n) = encoded_value
+        .strip_prefix('i')
+        .and_then(|s| s.strip_suffix('e')
+        .and_then(|n| n.parse::<isize>().ok()))
+    {
+        n.into()
+    } else if let Some((n, s)) = encoded_value
         .split_once(':')
         .and_then(|(n, s)| n.parse::<usize>().ok().map(|x| (x, s)))
     {
-        serde_json::Value::String(s[..n].to_string())
+        s[..n].into()
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -52,5 +58,11 @@ mod tests {
     #[test]
     fn decode_strings() {
         assert_eq!(decode_bencoded_value("3:abc"), "abc");
+    }
+
+    #[test]
+    fn decode_integers() {
+        assert_eq!(decode_bencoded_value("i467e"), 467);
+        assert_eq!(decode_bencoded_value("i-467e"), -467);
     }
 }
